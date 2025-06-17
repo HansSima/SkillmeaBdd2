9f4948c8bb4b9bbd9330162a94d1484f6a807a03
HansSima <SimaHonza@seznam.cz>
2025-06-17 19:50:47 +0200

    new test commit

---
+++ src/main/java/PokusBdd.java
@@ -0,0 +1,35 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PokusBdd {
+    @Given("nastaveni pocatecnich podminek")
+    public void nastaveniPocatecnichPodminek() {
+
+    }
+
+    @And("dalsi nastavovaci krok")
+    public void dalsiNastavovaciKrok() {
+    }
+
+    @When("toto je nejaka akce")
+    public void totoJeNejakaAkce() {
+
+    }
+
+    @And("toto je calsi akce")
+    public void totoJeCalsiAkce() {
+
+    }
+
+    @Then("overeni vysledku")
+    public void overeniVysledku() {
+
+    }
+
+    @And("dalsi overeni vysledku")
+    public void dalsiOvereniVysledku() {
+
+    }
+}
