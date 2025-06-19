c44c34275716264a3f5048bafee16229b8cb1e51
HansSima <SimaHonza@seznam.cz>
2025-06-19 19:00:01 +0200

    new test commit

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,12 +1,18 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import product.BankAccount;
 
 public class BankTest {
+
+    BankAccount firstBankAccount;
+
     @Given("Bank account with {int} kc")
-    public void bankAccountWithKc(int arg0) {
+    public void bankAccountWithKc(int initialBalance) {
+        firstBankAccount = new BankAccount(initialBalance);
     }
 
     @Then("A user account balance is {int} kc")
-    public void aUserAccountBalanceIsKc(int arg0) {
+    public void aUserAccountBalanceIsKc(int expectedBalance) {
+
     }
 }
