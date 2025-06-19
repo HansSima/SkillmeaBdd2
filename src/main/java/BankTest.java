2277315d96fab64ce55cab114934d90f4042153c
HansSima <SimaHonza@seznam.cz>
2025-06-19 18:47:27 +0200

    new test commit

---
+++ src/main/java/BankTest.java
@@ -0,0 +1,12 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class BankTest {
+    @Given("Bank account with {int} kc")
+    public void bankAccountWithKc(int arg0) {
+    }
+
+    @Then("A user account balance is {int} kc")
+    public void aUserAccountBalanceIsKc(int arg0) {
+    }
+}
